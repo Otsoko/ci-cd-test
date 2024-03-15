c9c9c13045e642609251c4ee822adb51cd66be50
JuanFer <juanfer@ai2.upv.es>
2024-03-15 02:03:52 +0100

    Java version compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,10 @@ android {
             )
         }
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
     kotlinOptions {
         jvmTarget = "17"
     }
